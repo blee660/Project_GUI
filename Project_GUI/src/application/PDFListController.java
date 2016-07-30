af70641e0789ea87c4c3e8c392d6aa4e71a758ed
chuan-yu <chuan1994@hotmail.com>
2016-07-30 17:05:27 +1200

    adding in PDFlist controller and linking methods

---
+++ Project_GUI/src/application/PDFListController.java
@@ -0,0 +1,15 @@
+package application;
+
+import javafx.event.ActionEvent;
+
+public class PDFListController {
+
+	public void addPDF(ActionEvent event){
+		System.out.println("added");
+	}
+	
+	public void removePDF(ActionEvent event){
+		System.out.println("removed");
+	}
+	
+}
