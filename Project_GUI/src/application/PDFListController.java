a14d1ac56a8213ac36aaa108ed96543015dc731b
chuan-yu <chuan1994@hotmail.com>
2016-07-30 18:02:28 +1200

    Filechooser logic added in to PDF list controller

--- Project_GUI/src/application/PDFListController.java
+++ Project_GUI/src/application/PDFListController.java
@@ -1,15 +1,39 @@
 package application;
 
+import java.io.File;
+import java.util.List;
+
 import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.FileChooser;
 
 public class PDFListController {
 
-	public void addPDF(ActionEvent event){
-		System.out.println("added");
+	private FileChooser fc = new FileChooser();
+
+	public PDFListController() {
+		setFilters();
 	}
-	
-	public void removePDF(ActionEvent event){
+
+	public void addPDF(ActionEvent event) {
+		
+		List<File> selectedFiles = fc.showOpenMultipleDialog(((Node) event.getSource()).getScene().getWindow());
+
+		if (selectedFiles != null) {
+			for (File f : selectedFiles) {
+				System.out.println(f.getAbsolutePath());
+			}
+		}
+	}
+
+	public void removePDF(ActionEvent event) {
 		System.out.println("removed");
 	}
-	
+
+	private void setFilters() {
+		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("PDF files (*.pdf)", ".pdf");
+		fc.getExtensionFilters().add(extFilter);
+		fc.setSelectedExtensionFilter(extFilter);
+	}
+
 }
