632777c4fe41437fdb42e41fd78ba5a5e7f44c85
chuan-yu <chuan1994@hotmail.com>
2016-08-31 20:16:40 +1200

    adding ability to add tabs and update tabs

--- Project_GUI/src/application/MainController.java
+++ Project_GUI/src/application/MainController.java
@@ -1,9 +1,29 @@
 package application;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+
+public class MainController implements Initializable{
+
+	@FXML
+	private TabPane tabPane;
+		
+	
 	public MainController(){
 		
 	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+	}
 	
+	public void addPane(Tab tab){
+		
+		tabPane.getTabs().add(tab);
+	}
 }
